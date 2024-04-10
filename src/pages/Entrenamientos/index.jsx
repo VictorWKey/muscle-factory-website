642160b7cc076cdc06c7946aa0d2cb060333528b
victorwkey <victorwkey@gmail.com>
2024-04-10 00:53:12 -0600

    Finish Muscle Factory web site

--- src/pages/Entrenamientos/index.jsx
+++ src/pages/Entrenamientos/index.jsx
@@ -8,9 +8,9 @@ function Entrenamientos() {
   return (
       <main id="top" className="w-full flex flex-col justify-center items-center sm:pt-0 bg-black">
         {/* HEADER */}
-        <div className="w-full h-auto flex max-sm:flex-col justify-center items-center gap-8 pb-16 pt-28 bg-black">
-          <div className='p-6'>
-            <figure className="size-72 xl:size-[420px] rounded-full  overflow-hidden">
+        <div className="w-full h-auto flex flex-col sm:flex-row justify-center items-center gap-6 sm:gap-8 pb-16 pt-28 bg-black">
+          <div className='sm:pl-6 xl:p-0'>
+            <figure className="size-72 xl:size-[340px] 2xl:size-[420px] rounded-full  overflow-hidden">
               <img
                 src="../../../public/img/entrenamientos/oscar-1.png"
                 alt="Foto de Victor"
@@ -18,13 +18,13 @@ function Entrenamientos() {
               />
             </figure>
           </div>
-          <div className="flex flex-col z-10 w-full max-w-3xl pl-6 pr-6 sm:pl-0 sm:pr-6 xl:pl-0">
+          <div className="flex flex-col z-10 w-full xl:max-w-2xl 2xl:max-w-3xl pl-6 pr-6 sm:pl-0 sm:pr-6 xl:pl-0">
 
-            <h1 className="text-3xl sm:text-5xl xl:text-8xl font-bold text-primarylight gap-2 text-pretty">OSCAR ÁVILA</h1>
+            <h1 className="text-3xl sm:text-5xl xl:text-7xl 2xl:text-8xl font-extrabold text-primarylight gap-2 text-pretty">OSCAR ÁVILA</h1>
 
             
-            <h2 className="text-base sm:text-lg xl:text-3xl font-extrabold text-pretty text-white mb-5">ENTRENADOR PERSONAL PROFESIONAL</h2>
-            <h3 className="font-normal text-base xl:text-2xl text-pretty text-white">Nuestro servicio de entrenamiento personalizado está diseñado para ayudarte a alcanzar tus objetivos de fitness de manera efectiva y segura. Trabajarás de cerca con nuestro entrenador certificado, Oscar Ávila, quien tiene más de 10 años de experiencia en el campo del fitness.</h3>
+            <h2 className="text-base sm:text-lg xl:text-2xl 2xl:text-3xl font-extrabold text-pretty text-white mb-5">ENTRENADOR PERSONAL PROFESIONAL</h2>
+            <h3 className="font-normal text-base xl:text-xl 2xl:text-2xl text-pretty text-white">Nuestro servicio de entrenamiento personalizado está diseñado para ayudarte a alcanzar tus objetivos de fitness de manera efectiva y segura. Trabajarás de cerca con nuestro entrenador certificado, Oscar Ávila, quien tiene más de 10 años de experiencia en el campo del fitness.</h3>
 
             <ul className="ml-1 sm:ml-0 mt-6 flex gap-6 items-center">
               <li>
@@ -35,7 +35,7 @@ function Entrenamientos() {
                   className="text-white transition hover:text-primarylight"
                 >
                   <span className="sr-only">Facebook</span>
-                  <svg className="size-9 sm:size-12" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <svg className="size-9 2xl:size-12" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                     <path
                       fillRule="evenodd"
                       d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
@@ -53,7 +53,7 @@ function Entrenamientos() {
                   className="text-white transition hover:text-primarylight"
                 >
                   <span className="sr-only">Instagram</span>
-                  <svg className="size-9 sm:size-12" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <svg className="size-9 2xl:size-12" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                     <path
                       fillRule="evenodd"
                       d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
@@ -71,7 +71,7 @@ function Entrenamientos() {
                   className="flex gap-1 items-center justify-center text-white font-semibold bg-primarylight rounded py-2 px-4 sm:px-8 transition duration-300 ease-in-out hover:bg-primarydark hover:scale-110"
                 >
                   
-                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-brand-whatsapp size-6 sm:size-7"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" /><path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" /></svg>
+                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-brand-whatsapp size-6 2xl:size-7"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" /><path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" /></svg>
                   <span>Contáctame</span>
                 </a>
               </li>
